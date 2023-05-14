02087aa72422d5cc3f73c957b535ed4e930db3a9
Rafael Treiber <rafaeltreiber@yahoo.com.br>
2023-05-14 20:21:10 -0300

    finishing login modal

---
+++ src/components/Navbar/RightContent/RightContent.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Flex } from '@chakra-ui/react';
+import AuthButtons from './AuthButtons';
+import AuthModal from '../../Modal/Auth/AuthModal';
+
+type RightContentProps = {
+  // user: any;
+
+};
+
+const RightContent: React.FC<RightContentProps> = () => {
+
+  return (
+    <>
+      <AuthModal />
+      <Flex justify='center' align='center'>
+        <AuthButtons />
+
+      </Flex>
+    </>
+  )
+}
+export default RightContent;
